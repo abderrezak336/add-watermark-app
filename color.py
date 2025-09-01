91d71c40e8827d6c981a5220bca827ba12608904
CodeWithAbdou <100610555+abderrezak336@users.noreply.github.com>
2025-09-01 18:06:32 +0100

    Initial commit via script

---
+++ color.py
@@ -0,0 +1,31 @@
+colors_rgb = {
+    "red": (255, 0, 0),
+    "blue": (0, 0, 255),
+    "green": (0, 255, 0),
+    "yellow": (255, 255, 0),
+    "purple": (128, 0, 128),
+    "orange": (255, 165, 0),
+    "pink": (255, 192, 203),
+    "brown": (139, 69, 19),
+    "black": (0, 0, 0),
+    "white": (255, 255, 255),
+    "grey": (128, 128, 128),
+    "cyan": (0, 255, 255),
+    "magenta": (255, 0, 255),
+    "lime": (0, 255, 0),
+    "teal": (0, 128, 128),
+    "navy": (0, 0, 128),
+    "maroon": (128, 0, 0),
+    "olive": (128, 128, 0),
+    "indigo": (75, 0, 130),
+    "violet": (238, 130, 238)
+}
+
+colors = [
+    "red", "blue", "green", "yellow", "purple",
+    "orange", "pink", "brown", "black", "white",
+    "grey", "cyan", "magenta", "lime", "teal",
+    "navy", "maroon", "olive", "indigo", "violet"
+]
+colors_tuple = tuple(colors)
+
